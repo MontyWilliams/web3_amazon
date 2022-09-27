1941b47fd4ef68672cb143391df7a7baeb3b3b6e
Montywilliams <3650@holbertonschool.com>
2022-09-27 10:26:23 -0600

    added images and used an api that takes a string and genaerates an image and returns it

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,63 @@
 import React from 'react'
+import Image from 'next/image'
 import { ConnectButton } from 'web3uikit'
 
+const isAuthenticated = true
+const username = 'Bro Bro'
+
 const Sidebar = () => {
+  const styles = {
+    container: `h-full w-[300px] flex flex-col bg-[#fff] static`,
+    profile: ` w-full py-16 flex flex-col justify-center items-center rounded-r-3xl bg-gradient-to-t from-[#0d141c] to-[#42667e] mt-[40px] mb-[50px] border-2 border-[#fb9701]`,
+    profilePicContainer: `flex  rounded-xl items-center justify-center w-full h-full mb-5`,
+    profilePic: `rounded-3xl object-cover`,
+    welcome: ` text-md mb-2 font-bold text-2xl text-white`,
+  
+  }
   return (
-    <ConnectButton />
+    <div className={styles.container}>
+      <div className={styles.profile}>
+        { //check if user is authenticated
+          isAuthenticated && (
+            <>
+            <div className={styles.profilePicContainer}>
+              <Image
+              src={`https://avatars.dicebear.com/api/pixel-art/${username}.svg`}
+              alt='profile'
+              className={styles.profilePic}
+              height={100}
+              width={100}
+              />
+            </div>
+            {!username ? (  // check for username if none show the input
+              <>
+                <div className={styles.username}>
+                <input 
+                type='text'
+                placeholder='Username...'
+                className={usernameInput}
+                // value ={nickname}
+                // onchange={e => setNickname(e.target.value)}
+                />
+                </div>
+                <button className={styles.setNickname}>
+                // onClick={handleSetUsername}
+                set Nickname
+                </button>
+              </>
+            ) : (
+              <div>
+              <div className={styles.welcome}>Welcome{username}</div>
+              </div>
+            )
+          }
+          </>
+          )
+        }
+
+      </div>
+
+    </div>
   )
 }
 
