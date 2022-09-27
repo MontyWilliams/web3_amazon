8cf2ca8c5afae573f5a2c5917a30e4ceff9988e5
Montywilliams <3650@holbertonschool.com>
2022-09-27 08:53:11 -0600

    added the server url and app id to provider via the enviornmental process

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,10 @@ import {MoralisProvider} from 'react-moralis '
 
 function MyApp({ Component, pageProps }) {
   return(
-    <MoralisProvider>
+    <MoralisProvider
+    serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL}
+    appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+    >
       <Component {...pageProps} />
     </MoralisProvider>
   )
