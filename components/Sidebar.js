a8e1193ba9e78ae5a6fef3876f3d3b5f11042b1f
Montywilliams <3650@holbertonschool.com>
2022-09-27 09:21:21 -0600

    added moralisV1 to dependencies because the updated version of Moralis dosnt work without it, wallet connect button added to sidebar=

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { ConnectButton} from 'web3uikit'
+import { ConnectButton } from 'web3uikit'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <ConnectButton />
   )
 }
 
