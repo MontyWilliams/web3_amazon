ff007131ccc9aee93983596c1eb1495bbda50e15
Montywilliams <3650@holbertonschool.com>
2022-09-27 10:43:15 -0600

    add styling and correct syntax

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { ConnectButton } from 'web3uikit'
 
 const isAuthenticated = true
-const username = 'Bro Bro'
+const username = ''
 
 const Sidebar = () => {
   const styles = {
@@ -12,6 +12,14 @@ const Sidebar = () => {
     profilePicContainer: `flex  rounded-xl items-center justify-center w-full h-full mb-5`,
     profilePic: `rounded-3xl object-cover`,
     welcome: ` text-md mb-2 font-bold text-2xl text-white`,
+    walletAddress: `text-xl flex w-full justify-center font-extrabold mb-4`,
+    menu: `flex flex-col w-full h-full px-10 gap-10`,
+    menuItem: `flex items-center text-lg font-bold cursor-pointer gap-2`,
+    amazonLogo: `mr-4 flex object-cover`,
+    companyName: `text-lg font-bold flex flex-1 pl-10 items-center mt-[20px]`,
+    usernameInput: `bg-transparent border-white border-2 rounded-lg w-[80%] py-2 px-4 text-lg mt-[20px] placeholder:text-white focus:outline-none flex justify-center items-center text-white`,
+    username: `flex items-center w-full justify-center`,
+    setNickname: `text-lg font-bold flex flex-1 items-center mt-[20px] mb-[20px] text-white`,
   
   }
   return (
@@ -35,13 +43,14 @@ const Sidebar = () => {
                 <input 
                 type='text'
                 placeholder='Username...'
-                className={usernameInput}
+                className={styles.usernameInput}
                 // value ={nickname}
                 // onchange={e => setNickname(e.target.value)}
                 />
                 </div>
-                <button className={styles.setNickname}>
+                <button className={styles.setNickname}
                 // onClick={handleSetUsername}
+                >
                 set Nickname
                 </button>
               </>
